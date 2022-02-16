eb0192c0eeda4fa696b4efa2559918523c102158
AwesomeCodingGuy <sonla@gmx.net>
2022-02-16 23:40:19 +0100

    Added mainwindow ui.

--- include/AwesomeNbtViewer/widgets/main_window.hpp
+++ include/AwesomeNbtViewer/widgets/main_window.hpp
@@ -4,13 +4,24 @@
 // Qt
 #include <QMainWindow>
 
+namespace Ui
+{
+class MainWindow;
+}
+
 namespace anv
 {
 
 class MainWindow : public QMainWindow
 {
+    Q_OBJECT
+
 public:
     MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *m_ui;
 };
 
 } // namespace anv
--- src/widgets/main_window.cpp
+++ src/widgets/main_window.cpp
@@ -1,12 +1,19 @@
 #include <AwesomeNbtViewer/widgets/main_window.hpp>
+#include <AwesomeNbtViewer/widgets/ui_main_window.h>
 
 namespace anv
 {
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
+    , m_ui(new Ui::MainWindow)
 {
+    m_ui->setupUi(this);
+}
 
+MainWindow::~MainWindow()
+{
+    delete m_ui;
 }
 
 } // namespace anv
