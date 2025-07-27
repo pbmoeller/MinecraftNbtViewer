1fb3031a15735bc78a469c50c2dea521171d8806
pbmoeller <sonla@gmx.net>
2025-07-27 13:41:20 +0200

    Split the code lines for reading.

--- src/util/minecraft_util.cpp
+++ src/util/minecraft_util.cpp
@@ -14,7 +14,12 @@ bool isMinecraftWorldDirectory(const QString& directory)
 {
     // Add more files to check if there are false results
     QFileInfo levelDatFile(directory + "/level.dat");
-    return levelDatFile.exists() && anvil::isNbtFile(levelDatFile.absoluteFilePath().toStdString());
+
+    if(levelDatFile.exists()) {
+        auto path = levelDatFile.absoluteFilePath().toStdString();
+        return anvil::isNbtFile(path);
+    }
+    return false;
 }
 
 } // namespace nbt
